9ca68fe809a41fc9f46378db1f8b7b0e90cd214d
SunayPatil <sunaypatil2003@gmail.com>
2023-10-07 22:14:27 +0530

    feedback page created

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,50 +1,230 @@
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import StarIcon from '@mui/icons-material/Star';
 import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
-// @mui
-import { Container, Stack, Typography } from '@mui/material';
-// components
-import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
-// mock
-import PRODUCTS from '../_mock/products';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { Button, Container, Typography } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import TextField from '@mui/material/TextField';
+import { wrap } from 'lodash';
 
-// ----------------------------------------------------------------------
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-export default function ProductsPage() {
-  const [openFilter, setOpenFilter] = useState(false);
+export default function HoverRating() {
+  const [breakfastRatings, setBreakfastRatings] = React.useState(null)
+  const [lunchRatings, setLunchRatings] = React.useState(null)
+  const [snacksRatings, setsnacksRatings] = React.useState(null)
+  const [dinnerRatings, setdinnerRatings] = React.useState(null)
+  const [hygieneRatings, sethygieneRatings] = React.useState(null)
+  const [messServiceRatings, setmessServiceRatings] = React.useState(null)
+  const [comments, setcomments] = React.useState(null)
 
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
+  const handleSubmitPress = ()=>{
+    const data = {
+      "breakfastRating" : breakfastRatings,
+      "lunchRatings" : lunchRatings,
+      "snacksRatings" : snacksRatings,
+      "dinnerRatings" : dinnerRatings,
+      "messServiceRatings" : messServiceRatings,
+      "hygieneRatings" : hygieneRatings,
+      "comments" : comments
+    }
 
-  const handleCloseFilter = () => {
-    setOpenFilter(false);
-  };
-
+    if(!breakfastRatings || !lunchRatings || !snacksRatings || !dinnerRatings || !hygieneRatings || !messServiceRatings){
+      alert("Please Rate all fields")
+    }
+    
+    else
+    console.log(data)
+    
+  }
   return (
     <>
-      <Helmet>
-        <title> Dashboard: Products | Minimal UI </title>
+    <Helmet>
+        <title> Feedback </title>
       </Helmet>
-
-      <Container>
-        <Typography variant="h4" sx={{ mb: 5 }}>
+      <Typography variant="h3" sx={{ mb: 1 }}>
           Feedback
-        </Typography>
-
-        {/* <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
-          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <ProductFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
+      </Typography>
+    
+    <Box sx={{ flexGrow: 1 , flexWrap: "wrap"}}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Item>
+            <Card sx={{ maxWidth: 345 }}>
+            <Container>
+            <Typography variant="h5" sx={{ mb: 1 }}>
+            Breakfast
+            </Typography>
+              <CardMedia
+              sx={{ height: 140 }}
+              image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
+              title=""
+              />
+            <Rating
+              name="breakfast-hover-feedback"
+              value={breakfastRatings}
+              precision={1}
+              onChange={(event, newValue) => {
+                setBreakfastRatings(newValue);
+              }}
+              emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
             />
-            <ProductSort />
-          </Stack>
-        </Stack> */}
+            </Container>
+            </Card>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <Card sx={{ maxWidth: 345 }}>
+              <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Lunch
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
+                    title=""
+                  />
+                  <Rating
+                    name="lunch-hover-feedback"
+                    value={lunchRatings}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      setLunchRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+              </Container>
+            </Card>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <Card sx={{ maxWidth: 345 }}>
+              <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Mess service
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL4DPJnMsIakcrye0Yjrip3JHV29UmMpN9xvj3eHrlfI4Co1kYHN75575xUKXwNan2ci8&usqp=CAU'
+                    title=""
+                  />
+                  <Rating
+                    name="lunch-hover-feedback"
+                    value={messServiceRatings}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      setmessServiceRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+              </Container>
+            </Card>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <Card sx={{ maxWidth: 345 }}>
+              <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Snacks
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
+                    title=""
+                  />
+                  <Rating
+                    name="lunch-hover-feedback"
+                    value={snacksRatings}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      setsnacksRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+              </Container>
+            </Card>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <Card sx={{ maxWidth: 345 }}>
+              <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Dinner
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
+                    title=""
+                  />
+                  <Rating
+                    name="lunch-hover-feedback"
+                    value={dinnerRatings}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      setdinnerRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+              </Container>
+            </Card>
+          </Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>
+          <Card sx={{ maxWidth: 345 }}>
+              <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Hygiene
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDqjJ4WNV0E0nmCGFLBZYUl_J6uOZyDMHKRA&usqp=CAU'
+                    title=""
+                  />
+                  <Rating
+                    name="lunch-hover-feedback"
+                    value={hygieneRatings}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      sethygieneRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+              </Container>
+            </Card>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
 
-        {/* <ProductList products={PRODUCTS} />
-        <ProductCartWidget /> */}
-      </Container>
+    <Box sx={{ flexGrow: 1 }}>
+      <Typography align='left' variant="h5" sx={{ mb: 1 }}>
+        Additional Comments
+      </Typography>
+      <TextField multiline rows={4} fullWidth id="outlined-basic" label="Comments" value={comments} variant="outlined"
+        onChange={(event) => {
+          setcomments(event.target.value);
+        }}
+      />
+    </Box>
+    
+      <Button variant="contained" onClick={handleSubmitPress}>Submit</Button>
+    
     </>
   );
 }
