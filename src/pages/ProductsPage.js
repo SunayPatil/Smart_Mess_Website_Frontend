71d925157efb06b1ea9a7dc63823f2b8424e0620
karthik-k-18 <200010024@iitdh.ac.in>
2023-10-08 11:41:03 +0530

    submit feedback-form integrated with backend

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -11,6 +11,7 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import TextField from '@mui/material/TextField';
 import { wrap } from 'lodash';
+import { submitFeedback } from '../utils/apis';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -21,73 +22,76 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function HoverRating() {
-  const [breakfastRatings, setBreakfastRatings] = React.useState(null)
-  const [lunchRatings, setLunchRatings] = React.useState(null)
-  const [snacksRatings, setsnacksRatings] = React.useState(null)
-  const [dinnerRatings, setdinnerRatings] = React.useState(null)
-  const [hygieneRatings, sethygieneRatings] = React.useState(null)
-  const [messServiceRatings, setmessServiceRatings] = React.useState(null)
-  const [comments, setcomments] = React.useState(null)
+  const [BreakfastRating, setBreakfastRatings] = React.useState(null);
+  const [LunchRating, setLunchRatings] = React.useState(null);
+  const [SnacksRating, setsnacksRatings] = React.useState(null);
+  const [DinnerRating, setdinnerRatings] = React.useState(null);
+  const [HygieneRating, sethygieneRatings] = React.useState(null);
+  const [MessServiceRating, setmessServiceRatings] = React.useState(null);
+  const [Comments, setcomments] = React.useState(null);
 
-  const handleSubmitPress = ()=>{
-    const data = {
-      "breakfastRating" : breakfastRatings,
-      "lunchRatings" : lunchRatings,
-      "snacksRatings" : snacksRatings,
-      "dinnerRatings" : dinnerRatings,
-      "messServiceRatings" : messServiceRatings,
-      "hygieneRatings" : hygieneRatings,
-      "comments" : comments
+  const handleSubmitPress = async () => {
+    if (!BreakfastRating || !LunchRating || !SnacksRating || !DinnerRating || !HygieneRating || !MessServiceRating) {
+      alert('Please Rate all fields');
+    } else {
+      const res = await submitFeedback({
+        FormID: '1',
+        BreakfastRating,
+        LunchRating,
+        SnacksRating,
+        DinnerRating,
+        HygieneRating,
+        MessServiceRating,
+        Comments,
+      });
+      console.log(res);
+      if (res.status === 200) {
+        alert('Feedback Submitted');
+      } else {
+        alert('Feedback Submission Failed');
+      }
     }
-
-    if(!breakfastRatings || !lunchRatings || !snacksRatings || !dinnerRatings || !hygieneRatings || !messServiceRatings){
-      alert("Please Rate all fields")
-    }
-    
-    else
-    console.log(data)
-    
-  }
+  };
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title> Feedback </title>
       </Helmet>
       <Typography variant="h3" sx={{ mb: 1 }}>
-          Feedback
+        Feedback
       </Typography>
-    
-    <Box sx={{ flexGrow: 1 , flexWrap: "wrap"}}>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <Item>
-            <Card sx={{ maxWidth: 345 }}>
-            <Container>
-            <Typography variant="h5" sx={{ mb: 1 }}>
-            Breakfast
-            </Typography>
-              <CardMedia
-              sx={{ height: 140 }}
-              image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
-              title=""
-              />
-            <Rating
-              name="breakfast-hover-feedback"
-              value={breakfastRatings}
-              precision={1}
-              onChange={(event, newValue) => {
-                setBreakfastRatings(newValue);
-              }}
-              emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
-            />
-            </Container>
-            </Card>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <Card sx={{ maxWidth: 345 }}>
-              <Container>
+
+      <Box sx={{ flexGrow: 1, flexWrap: 'wrap' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
+                  <Typography variant="h5" sx={{ mb: 1 }}>
+                    Breakfast
+                  </Typography>
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="https://media.istockphoto.com/id/938158500/photo/breakfast-table.jpg?s=612x612&w=0&k=20&c=Y8xB6hfe4dSPNyNrPgzP7slHbKhWdEzG7YTk2WXu4lQ="
+                    title=""
+                  />
+                  <Rating
+                    name="breakfast-hover-feedback"
+                    value={BreakfastRating}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                      setBreakfastRatings(newValue);
+                    }}
+                    emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
+                  />
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
                   <Typography variant="h5" sx={{ mb: 1 }}>
                     Lunch
                   </Typography>
@@ -98,46 +102,46 @@ export default function HoverRating() {
                   />
                   <Rating
                     name="lunch-hover-feedback"
-                    value={lunchRatings}
+                    value={LunchRating}
                     precision={1}
                     onChange={(event, newValue) => {
                       setLunchRatings(newValue);
                     }}
                     emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
                   />
-              </Container>
-            </Card>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <Card sx={{ maxWidth: 345 }}>
-              <Container>
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
                   <Typography variant="h5" sx={{ mb: 1 }}>
                     Mess service
                   </Typography>
                   <CardMedia
                     sx={{ height: 140 }}
-                    image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL4DPJnMsIakcrye0Yjrip3JHV29UmMpN9xvj3eHrlfI4Co1kYHN75575xUKXwNan2ci8&usqp=CAU'
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL4DPJnMsIakcrye0Yjrip3JHV29UmMpN9xvj3eHrlfI4Co1kYHN75575xUKXwNan2ci8&usqp=CAU"
                     title=""
                   />
                   <Rating
                     name="lunch-hover-feedback"
-                    value={messServiceRatings}
+                    value={MessServiceRating}
                     precision={1}
                     onChange={(event, newValue) => {
                       setmessServiceRatings(newValue);
                     }}
                     emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
                   />
-              </Container>
-            </Card>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <Card sx={{ maxWidth: 345 }}>
-              <Container>
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
                   <Typography variant="h5" sx={{ mb: 1 }}>
                     Snacks
                   </Typography>
@@ -148,21 +152,21 @@ export default function HoverRating() {
                   />
                   <Rating
                     name="lunch-hover-feedback"
-                    value={snacksRatings}
+                    value={SnacksRating}
                     precision={1}
                     onChange={(event, newValue) => {
                       setsnacksRatings(newValue);
                     }}
                     emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
                   />
-              </Container>
-            </Card>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <Card sx={{ maxWidth: 345 }}>
-              <Container>
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
                   <Typography variant="h5" sx={{ mb: 1 }}>
                     Dinner
                   </Typography>
@@ -173,58 +177,66 @@ export default function HoverRating() {
                   />
                   <Rating
                     name="lunch-hover-feedback"
-                    value={dinnerRatings}
+                    value={DinnerRating}
                     precision={1}
                     onChange={(event, newValue) => {
                       setdinnerRatings(newValue);
                     }}
                     emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
                   />
-              </Container>
-            </Card>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-          <Card sx={{ maxWidth: 345 }}>
-              <Container>
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 345 }}>
+                <Container>
                   <Typography variant="h5" sx={{ mb: 1 }}>
                     Hygiene
                   </Typography>
                   <CardMedia
                     sx={{ height: 140 }}
-                    image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDqjJ4WNV0E0nmCGFLBZYUl_J6uOZyDMHKRA&usqp=CAU'
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDqjJ4WNV0E0nmCGFLBZYUl_J6uOZyDMHKRA&usqp=CAU"
                     title=""
                   />
                   <Rating
                     name="lunch-hover-feedback"
-                    value={hygieneRatings}
+                    value={HygieneRating}
                     precision={1}
                     onChange={(event, newValue) => {
                       sethygieneRatings(newValue);
                     }}
                     emptyIcon={<StarIcon style={{ opacity: 0.45 }} fontSize="inherit" />}
                   />
-              </Container>
-            </Card>
-          </Item>
+                </Container>
+              </Card>
+            </Item>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
 
-    <Box sx={{ flexGrow: 1 }}>
-      <Typography align='left' variant="h5" sx={{ mb: 1 }}>
-        Additional Comments
-      </Typography>
-      <TextField multiline rows={4} fullWidth id="outlined-basic" label="Comments" value={comments} variant="outlined"
-        onChange={(event) => {
-          setcomments(event.target.value);
-        }}
-      />
-    </Box>
-    
-      <Button variant="contained" onClick={handleSubmitPress}>Submit</Button>
-    
+      <Box sx={{ flexGrow: 1 }}>
+        <Typography align="left" variant="h5" sx={{ mb: 1 }}>
+          Additional Comments
+        </Typography>
+        <TextField
+          multiline
+          rows={4}
+          fullWidth
+          id="outlined-basic"
+          label="Comments"
+          value={Comments}
+          variant="outlined"
+          onChange={(event) => {
+            setcomments(event.target.value);
+          }}
+        />
+      </Box>
+
+      <Button variant="contained" onClick={handleSubmitPress}>
+        Submit
+      </Button>
     </>
   );
 }
