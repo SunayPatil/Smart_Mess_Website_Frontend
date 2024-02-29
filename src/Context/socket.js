a0067b8ff13f30d893beae98902051124ccc7a12
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2024-02-29 19:08:03 +0530

    socket update

--- src/Context/socket.js
+++ src/Context/socket.js
@@ -2,5 +2,5 @@ import React from "react";
 import io from "socket.io-client";
 
 
-export const socket = io.connect("http://localhost:8000");
+export const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
 export const SocketContext = React.createContext();
