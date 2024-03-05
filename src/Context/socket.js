f65544372145f698ca992dd1bdb99c0743db40c4
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2024-03-05 12:41:55 +0530

    testing socket on server

--- src/Context/socket.js
+++ src/Context/socket.js
@@ -1,6 +1,7 @@
-import React from "react";
-import io from "socket.io-client";
+import React from 'react';
+import io from 'socket.io-client';
 
-
-export const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
+export const socket = io.connect(process.env.REACT_APP_SOCKET_URL, {
+  transports: ['websocket'], // forces websockets only
+});
 export const SocketContext = React.createContext();
