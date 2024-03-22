5cd8fa3cb6551241c2dd4ffd171815230a4dbe30
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2024-03-22 13:58:47 +0530

    testing ws

--- src/Context/socket.js
+++ src/Context/socket.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
+export const socket = io.connect(process.env.REACT_APP_SOCKET_URL, {
+  path: '/socket.io',
+  autoConnect: false,
+  transports: ['websocket'],
+});
 socket.on('connect_error', (err) => {
   // the reason of the error, for example "xhr poll error"
   console.log(err.message);
